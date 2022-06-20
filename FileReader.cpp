2d1d207d6a278a598da67d2089446426881a3c9f
JustinCredibleStudios <justincredible219@gmail.com>
2022-06-19 21:42:55 -0400

    Final Project

--- FileReader.cpp
+++ FileReader.cpp
@@ -5,5 +5,40 @@
 using namespace std;
 
 int main() {
+    // Vars to Accomplish tasks
+    ifstream inputFile;
+    ofstream outputFile;
+    string fileName;
+
+    // vars for each person
+    string firstName;
+    string lastName;
+    double oldSalary;
+    double newSalary;
+    double payIncrease;
+
+    // Pronpt User for file name
+    cout << "File Name: ";
+    cin >> fileName;
+
+    // Openning the file
+    inputFile.open(fileName);
+    outputFile.open("input.dat");
+
+    // 'Set percision label on output file
+    outputFile << fixed << showpoint << setprecision(2);
+
+    // Loop through input and write to output
+    while (!inputFile.eof()) {
+        inputFile >> lastName >> firstName >> oldSalary >> payIncrease;
+        newSalary = oldSalary + (oldSalary * payIncrease) / 100;
+        outputFile << firstName << ' ' << lastName << ' ' << newSalary << endl;
+    }
+
+    // Close both Files & exit the program
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
 
 }
